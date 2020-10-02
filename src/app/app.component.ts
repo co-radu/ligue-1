fedaaf8b1162893bd8b9668b682175520eb78fba
Radureau Corentin <radureau.c@gmail.com>
2020-10-02 18:47:43 +0200

    Addition of a social bar and a toolbar (desk and tablet).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,30 +2,36 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
-  constructor(
-    private router: Router,
-  ) { }
+	constructor(
+		private router: Router,
+	) { }
 
-  redirectTo(destination: string): void {
-    switch (destination) {
-      case 'Linkedin':
-        window.open('https://linkedin.com/in/corentin-radureau-93289a139', '_blank');
-        break;
-      case 'Github':
-        window.open('https://github.com/co-radu', '_blank');
-        break;
-      case 'Accueil':
-        this.router.navigate(['']);
-        break;
-      case 'Classement':
-        this.router.navigate(['/ranking']);
-        break;
-    }
-  }
+	redirectTo(destination: string): void {
+		switch (destination) {
+			case 'lfp':
+				window.open('https://www.lfp.fr/fr-FR', '_blank');
+				break;
+			case 'fff':
+				window.open('https://www.fff.fr/?home', '_blank');
+				break;
+			case 'linkedin':
+				window.open('https://linkedin.com/in/corentin-radureau-93289a139', '_blank');
+				break;
+			case 'github':
+				window.open('https://github.com/co-radu', '_blank');
+				break;
+			case 'accueil':
+				this.router.navigate(['']);
+				break;
+			case 'classement':
+				this.router.navigate(['/ranking']);
+				break;
+		}
+	}
 }
